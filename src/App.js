e19d933b77adef2d701e1c5ffdd0a96e57f2079f
surendrareddygaddam <your.email@example.com>
2024-09-05 11:38:10 +0530

    json data added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [data,setData]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setData(data))
+  },[])
   
   return (
     <div>
       <h1>Git main branch</h1>
       <h2>Hello some changes has been added to the git hub branch Added</h2>
       <h3>Added by the git repository</h3>
+      {JSON.stringify(data)}
     </div>
   )
 }
