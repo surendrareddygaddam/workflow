e809a87639528333dc71c2c5718a9be00e15e6a9
surendrareddygaddam <your.email@example.com>
2024-09-05 10:37:13 +0530

    added by the git branchadded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [data,setData]=useState([])
+  
   return (
     <div>
       <h1>Git main branch</h1>
+      <h2>Hello some changes has been added to the git hub branch Added</h2>
+
     </div>
   )
 }
